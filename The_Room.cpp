f25012b2a981fa8e3e1e38708717f5865318a7d4
Md. Hafizur Rahman Arfin <arfin97@gmail.com>
2019-06-22 18:49:50 +0630

    Arfin Finalized.
    - Wall-mat 
    - Small Drawer
    - Vase+Flower
    - Big Drawers
    - Monitor+Keyboard+Mouse
    - Chair 1
    - Mat

--- The_Room.cpp
+++ The_Room.cpp
@@ -1,7 +1,20 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include<math.h>
 double hh = 1000, ww = 1000;
 //int d=20,p,gx=0,hy=0,hxc=495,gyc=795,ha,gb;
+
+void drawCircle(int xx, int yy, int rad, int r, int g, int b){
+
+    glColor3ub(r, g, b);
+    glBegin(GL_POLYGON);
+    for(int i = 0; i < 360; i++){
+        float theta = i*3.142/180;
+        glVertex2f(xx+rad*cos(theta), yy+rad*sin(theta));
+    }
+    glEnd();
+}
+
 
 void showGrid(double h, double w, double dis){
 /// Page grid vertical
@@ -269,8 +282,403 @@ void display(void) {
     glVertex2d(380, 330);
     glEnd();
 
+/// GRID
+    showGrid(hh, ww, 20);
+/// GRID
 
-    showGrid(hh, ww);
+
+    /*
+    ####################################################################################################################
+    ##############################----------------------ARFIN-----------------------------##############################
+    ####################################################################################################################
+    */
+
+////////////////////////////////////////////////--------Wallmat START-------------/////////////////////////////
+/// Wallmat background
+    glColor3d(0.15, 0.19, 0.22);
+    glBegin(GL_POLYGON);
+    glVertex2d(220, 640);
+    glVertex2d(220, 760);
+    glVertex2d(320, 860);
+    glVertex2d(320, 740);
+    glEnd();
+
+
+/// Wallmat canvas
+    glColor3d(0.15, 0.19, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(220, 660);
+    glVertex2d(220, 740);
+    glVertex2d(320, 840);
+    glVertex2d(320, 760);
+    glEnd();
+
+ /// Circle
+    drawCircle(277,739,15, 255,255,0);
+
+/// Hill Left
+    glColor3ub(255, 0, 0);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(220, 660);
+    glVertex2d(250, 740);
+    glVertex2d(280, 720);
+    glEnd();
+
+/// Hill Right
+    glColor3d(1, 0, 0);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(280, 720);
+    glVertex2d(295, 800);
+    glVertex2d(320, 760);
+    glEnd();
+////////////////////////////////////////////////--------Wallmat-END -------------/////////////////////////////
+
+
+////////////////////////////////////////////////-------- SMALL BOXDESK START-------------/////////////////////////////
+/// BOXDESK FRONT
+    glColor3d(0, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(580, 420);
+    glVertex2d(580, 500);
+    glVertex2d(660, 500);
+    glVertex2d(660, 420);
+    glEnd();
+
+/// BOXDESK DRAWER
+    glColor3d(1, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(585, 425);
+    glVertex2d(585, 495);
+    glVertex2d(655, 495);
+    glVertex2d(655, 425);
+    glEnd();
+
+    drawCircle(645,460,5, 255, 255, 255);
+
+/// BOXDESK RIGHT SIDE
+    glColor3d(0, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(660, 420);
+    glVertex2d(660, 500);
+    glVertex2d(680, 520);
+    glVertex2d(680, 440);
+    glEnd();
+
+/// BOXDESK TOP
+    glColor3d(0, 1, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(580, 500);
+    glVertex2d(600, 520);
+    glVertex2d(680, 520);
+    glVertex2d(660, 500);
+    glEnd();
+
+/// VASE TOP
+    glColor3d(1, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(630, 510);
+    glVertex2d(620, 560);
+    glVertex2d(660, 560);
+    glVertex2d(650, 510);
+    glEnd();
+
+
+
+/// VASE TOP
+    glColor3d(1, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(640, 560);
+    glVertex2d(640, 600);
+    glVertex2d(645, 600);
+    glVertex2d(645, 560);
+    glEnd();
+
+
+
+
+int flower_r, flower_g, flower_b;
+flower_r = 0;
+flower_g = 0;
+flower_b = 255;
+
+ /// Flower Circle Background
+    drawCircle(642,600,15, flower_r,flower_g,flower_b);
+
+
+
+/// TRIANGE Left Bottom
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(620, 580);
+    glVertex2d(640, 600);
+    glVertex2d(640, 580);
+    glEnd();
+
+
+/// TRIANGE Left Top
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(620, 620);
+    glVertex2d(640, 600);
+    glVertex2d(640, 620);
+    glEnd();
+
+/// TRIANGE Left
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(630, 610);
+    glVertex2d(620, 600);
+    glVertex2d(630, 590);
+    glEnd();
+
+
+/// TRIANGE Right Top
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(640, 620);
+    glVertex2d(640, 600);
+    glVertex2d(660, 620);
+    glEnd();
+
+/// TRIANGE Right Bottom
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(640, 580);
+    glVertex2d(640, 600);
+    glVertex2d(660, 580);
+    glEnd();
+
+/// TRIANGE RIGHT
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(650, 610);
+    glVertex2d(660, 600);
+    glVertex2d(650, 590);
+    glEnd();
+
+/// TRIANGE Center TOP
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(630, 600);
+    glVertex2d(640, 640);
+    glVertex2d(650, 600);
+    glEnd();
+
+/// TRIANGE Center Bottom
+    glColor3ub(flower_r,flower_g,flower_b);
+    glBegin(GL_TRIANGLES);
+    glVertex2d(630, 600);
+    glVertex2d(640, 560);
+    glVertex2d(650, 600);
+    glEnd();
+
+
+    //////////////////////////////////////////////////////////------------BIG DESK LEFT----------///////////////////////////
+
+/// DESK FRONT
+    glColor3d(1, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(440, 420);
+    glVertex2d(440, 740);
+    glVertex2d(540, 740);
+    glVertex2d(540, 420);
+    glEnd();
+
+/// DESK RIGHT SIDE
+    glColor3d(1, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(540, 420);
+    glVertex2d(540, 740);
+    glVertex2d(560, 760);
+    glVertex2d(560, 440);
+    glEnd();
+
+/// DESK TOP SIDE
+    glColor3d(0, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(440, 740);
+    glVertex2d(460, 760);
+    glVertex2d(560, 760);
+    glVertex2d(540, 740);
+    glEnd();
+
+/// DESK middle Border
+    glColor3d(0, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(440, 540);
+    glVertex2d(440, 550);
+    glVertex2d(540, 550);
+    glVertex2d(540, 540);
+    glEnd();
+
+/// DESK middle Border
+    glColor3d(0, 1, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(480, 540);
+    glVertex2d(480, 550);
+    glVertex2d(500, 550);
+    glVertex2d(500, 540);
+    glEnd();
+
+/// DESK Stripe Border
+    glColor3d(0, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(485, 540);
+    glVertex2d(485, 740);
+    glVertex2d(490, 740);
+    glVertex2d(490, 540);
+    glEnd();
+
+/// DESK Stripe Border
+    glColor3d(0, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(490, 540);
+    glVertex2d(490, 740);
+    glVertex2d(495, 740);
+    glVertex2d(495, 540);
+    glEnd();
+
+
+
+    drawCircle(470,640,5, flower_r,flower_g,flower_b);
+
+    drawCircle(510,640,5, flower_r,flower_g,flower_b);
+
+
+/// DESK BOTTOM DRAWER 1
+    glColor3d(.55, .33, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(450, 440);
+    glVertex2d(450, 480);
+    glVertex2d(530, 480);
+    glVertex2d(530, 440);
+    glEnd();
+
+    drawCircle(490,460,5, flower_r,flower_g,flower_b);
+
+/// DESK BOTTOM DRAWER 2
+    glColor3d(.55, .33, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(450, 485);
+    glVertex2d(450, 525);
+    glVertex2d(530, 525);
+    glVertex2d(530, 485);
+    glEnd();
+
+    drawCircle(490,500,5, flower_r,flower_g,flower_b);
+
+
+// MAT
+    glColor3d(.55, .33, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(520, 0);
+    glVertex2d(560, 60);
+    glVertex2d(660, 60);
+    glVertex2d(680, 0);
+    glEnd();
+
+///////////////////////////////////////////*************MONITOR***************////////////////////
+// MAT
+/// Monitor Border
+    glColor3d(.55, .33, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(280, 420);
+    glVertex2d(280, 500);
+    glVertex2d(380, 500);
+    glVertex2d(380, 420);
+    glEnd();
+
+/// Monitor Screen
+    glColor3d(.55, 0, .33);
+    glBegin(GL_POLYGON);
+    glVertex2d(285, 425);
+    glVertex2d(285, 495);
+    glVertex2d(375, 495);
+    glVertex2d(375, 425);
+    glEnd();
+
+/// Monitor Neck
+    glColor3d(.55, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(320, 400);
+    glVertex2d(320, 420);
+    glVertex2d(340, 420);
+    glVertex2d(340, 400);
+    glEnd();
+
+/// Monitor Stand
+    glColor3d(.55, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(300, 380);
+    glVertex2d(310, 400);
+    glVertex2d(350, 400);
+    glVertex2d(360, 380);
+    glEnd();
+
+/// Keyboard
+    glColor3d(.55, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(270, 340);
+    glVertex2d(280, 360);
+    glVertex2d(370, 360);
+    glVertex2d(360, 340);
+    glEnd();
+
+    drawCircle(380,360,5, flower_r,flower_g,flower_b);
+
+///////////////////////////////////////////////-----TABLE 1 CHAIR-----------////////////////////////
+
+/// Chair top
+    glColor3d(.55, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(240, 300);
+    glVertex2d(240, 380);
+    glVertex2d(300, 380);
+    glVertex2d(300, 300);
+    glEnd();
+
+/// Chair seat
+    glColor3d(.55, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(240, 240);
+    glVertex2d(260, 280);
+    glVertex2d(320, 280);
+    glVertex2d(300, 240);
+    glEnd();
+
+/// Chair backbone
+    glColor3d(.55, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex2d(265, 180);
+    glVertex2d(265, 360);
+    glVertex2d(275, 360);
+    glVertex2d(275, 180);
+    glEnd();
+
+/// Chair bottom bumper
+    glColor3d(1, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(240, 180);
+    glVertex2d(240, 190);
+    glVertex2d(300, 190);
+    glVertex2d(300, 180);
+    glEnd();
+
+/// Chair wheels
+    drawCircle(240,180,5, flower_r,flower_g,flower_b);
+
+    drawCircle(300,180,5, flower_r,flower_g,flower_b);
+
+/// Chair bottom bumper
+    glColor3d(1, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2d(245, 160);
+    glVertex2d(265, 180);
+    glVertex2d(275, 180);
+    glVertex2d(255, 160);
+    glEnd();
+
+    drawCircle(250,160,5, flower_r,flower_g,flower_b);
 
 //
 ////wall left-right
@@ -1257,3 +1665,4 @@ int main(int argc, char** argv) {
     glutMainLoop();
     return 0; /* ISO C requires main to return int. */
 }
+
